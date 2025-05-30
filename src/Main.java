4d90dd7b13a295a30751c627334e25a0906f5c31
USER <rhs9827@naver.com>
2025-05-30 14:35:29 +0900

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        int age = 25;
 
+        System.out.println(age);
+
+        age = 26;
+
+        System.out.println(age);
+
+        double salary = 50000.75;
+
+        System.out.println(salary);
+
+        String name = "노현수";
+
+        System.out.println(name);
+        System.out.println(name.length());
     }
 }
